e8c2e34c2ca94f26cf421f99fea1f2171788c2b1
Tarun Gunaseelan <Tarun Gunaseelan>
2024-02-14 21:28:57 -0500

    initial template frontend

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,74 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { autolinkConfig } from "./plugins/rehype-autolink-config";
+import rehypeSlug from "rehype-slug";
+import astroI18next from "astro-i18next";
+import alpinejs from "@astrojs/alpinejs";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://v3dgt7fz-4321.use.devtunnels.ms/",
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [
+		tailwind(),
+		sitemap(),
+		astroI18next(),
+		alpinejs(),
+		AstroPWA({
+			mode: "production",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "currydumpster - official",
+				short_name: "currydumpster",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["*.js"],
+			},
+			devOptions: {
+				enabled: false,
+				navigateFallbackAllowlist: [/^\/404$/],
+				suppressWarnings: true,
+			},
+		}),
+	],
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+	},
+	experimental: {
+		contentCollectionCache: true,
+	}
+});
---
+++ frontend/src/content/blog/en/test.md
@@ -0,0 +1,25 @@
+---
+title: Test
+draft: false
+author: Tarun Gunaseelan
+tags:
+  - Test
+  - Really
+  - testing
+image:
+  src: /images/tik-tok-ads.png
+  alt: Test
+snippet: In the world of social media marketing, a new player is making a
+  significant impact TikTok. This guide explores the potential of TikTok ads and
+  how to harness them effectively
+publishDate: 2024-14-02 11:39
+category: Tech
+---
+
+
+In the world.
+
+
+## Why?
+
+IDK
